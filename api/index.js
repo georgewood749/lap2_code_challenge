da57c364adbf460d01dcfbb5b2c02fa6d84abefd
georgewood749 <georgewood749@gmail.com>
2022-10-19 16:16:26 +0100

    set up express

--- api/index.js
+++ api/index.js
@@ -1,17 +1,24 @@
-const postEntry = async (title, pseudonym, bodyText, timestamp) => {
-    await fetch(``, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "title": title,
-            "pseudonym": pseudonym,
-            "bodyText": bodyText,
-            "timestamp": timestamp
-        })
-    })
-}
+const app = require('./server');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Express now departing from port ${port}!`))
+
+
+// const postEntry = async (title, pseudonym, bodyText, timestamp) => {
+//     await fetch(``, {
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({
+//             "title": title,
+//             "pseudonym": pseudonym,
+//             "bodyText": bodyText,
+//             "timestamp": timestamp
+//         })
+//     })
+// }
 
 
 // function textCounter(postBox, counter, charLimit) {
