4114c2ad7742f12e6f0cd48a07064ab1f9a40e6f
kaiyuleung <kyl1g16.ecs@gmail.com>
2022-10-19 16:38:45 +0100

    docker and api set. api still have some properties mis-defined.

--- api/dbConfig/db_seed.sql
+++ api/dbConfig/db_seed.sql
@@ -1,20 +1,20 @@
-INSERT INTO posts  (author, dateTime, title, content)
+INSERT INTO posts (author, dateTime, title, content)
 VALUES
 (
     'Anyms1',
     '1000-01-01 00:00:00',
-    'TESTING TITLE'
+    'TESTING TITLE',
     $str$TESTING CONTENTS$str$
 ),
 (
     'Anyms2',
     '9999-12-31 23:59:59',
-    'this is varchar(255)'
+    'this is varchar(255)',
     $str$this is text$str$
 ),
 (
     'kai',
     '2022-10-19 16:10:59',
-    'ha'
+    'ha',
     $str$this is funny$str$
 );
