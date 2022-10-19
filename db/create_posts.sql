af47b7f400d0c3b78c843b080f54feb49b9a9a9e
georgewood749 <georgewood749@gmail.com>
2022-10-19 16:41:00 +0100

    tweaks to db seed

--- db/create_posts.sql
+++ db/create_posts.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts (
     id serial PRIMARY KEY,
-    title varchar(255) NOT NULL
+    title varchar(255) NOT NULL,
     pseudonym varchar(50) NOT NULL,
     bodyText varchar NOT NULL,
 );
